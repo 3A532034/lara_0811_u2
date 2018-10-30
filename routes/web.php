4ecbe44e91b7b274831336a7a8003aee83c300a9
吳駿松 <jo32229hn@gmail.com>
2018-10-31 00:38:39 +0800

    admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('say/{name?}',['as'=>'hello.index',function($name='Everybody'){
-    return 'Hello,'.$name;
-}]);
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return 'admin dashboard';
+    });
+});
